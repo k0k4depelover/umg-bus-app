b4f6ed4675436cd0b9ec7a9b1fbca3947fd7056b
k0k4depelover <oskarcermeno1@gmail.com>
2026-03-28 15:30:02 -0600

    feature: Adding guide to run project with android studio

---
+++ mobile/jest.setup.js
@@ -0,0 +1,34 @@
+jest.mock('react-native-gesture-handler', () => {
+  return {
+    GestureHandlerRootView: ({ children }) => children,
+    Swipeable: jest.fn(),
+    DrawerLayout: jest.fn(),
+    State: {},
+    PanGestureHandler: jest.fn(),
+    TapGestureHandler: jest.fn(),
+    FlingGestureHandler: jest.fn(),
+    ForceTouchGestureHandler: jest.fn(),
+    LongPressGestureHandler: jest.fn(),
+    NativeViewGestureHandler: jest.fn(),
+    RotationGestureHandler: jest.fn(),
+    ScrollView: jest.fn(),
+    Directions: {},
+  };
+});
+
+jest.mock('react-native-safe-area-context', () => {
+  const inset = { top: 0, right: 0, bottom: 0, left: 0 };
+  return {
+    SafeAreaProvider: ({ children }) => children,
+    SafeAreaView: ({ children }) => children,
+    useSafeAreaInsets: () => inset,
+  };
+});
+
+jest.mock('react-native-screens', () => ({
+  enableScreens: jest.fn(),
+}));
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
